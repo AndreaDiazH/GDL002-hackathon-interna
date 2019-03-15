efaaf33fe930c5828c2f163db039c8e6167b13af
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-03-15 10:31:42 -0600

    se agrego funcionalidad en main.js y se integro  el codigo de data.js para que haga el mostrado de la información, más estilos en css y elementos de bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-function showFirstScreen(){
+function showFirstScreen(Mood){
+ 
+  document.getElementById(Mood).style.display = "block";
    document.getElementById("howYouFeel").style.display = "none";
    document.getElementById("showMovies").style.display = "block";
 }
-document.getElementById("btnSmile").addEventListener('click',showFirstScreen);
-document.getElementById("btnSquint").addEventListener('click',showFirstScreen);
-document.getElementById("btnSad").addEventListener('click',showFirstScreen);
+document.getElementById("btnSmile").addEventListener('click',()=>{showFirstScreen("movieCardsSmile")});
+document.getElementById("btnSquint").addEventListener('click',()=>{showFirstScreen("movieCardsBored")});
+document.getElementById("btnSad").addEventListener('click',()=>{showFirstScreen("movieCardsSad")});
 
-//Esta funcion solo oculta el contenido principal para mostrar el siguiente.
+//Esta funcion solo oculta el contenido principal para mostrar el siguiente, responde a los tres botones que tenemos y muestra la data que corresponde a cada uno.
+
+function goBack(){
+  document.getElementById("showMovies").style.display = 'none';
+  document.getElementById("howYouFeel").style.display = 'block';
+}
+document.getElementById("btnBack").addEventListener('click',goBack);
+
+//Esta funcion responde al boton "volver" para regresar a la pantalla inicial.
